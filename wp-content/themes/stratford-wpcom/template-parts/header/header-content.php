949ee72340c68879a209a133bc46347f087b71fd
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2025-04-28 11:40:32 -0700

    added stratford free theme

---
+++ wp-content/themes/stratford-wpcom/template-parts/header/header-content.php
@@ -0,0 +1,7 @@
+<?php declare( strict_types = 1 ); ?>
+<header id="masthead" class="site-header alignfull">
+	<div class="site-header-wrapper">
+		<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
+		<?php get_template_part( 'template-parts/header/site', 'navigation' ); ?>
+	</div>
+</header><!-- #masthead -->
