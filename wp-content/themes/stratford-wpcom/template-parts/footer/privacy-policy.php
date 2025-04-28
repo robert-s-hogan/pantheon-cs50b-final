e08a6877a14de03f387a96b6b83cc6c6196d37cc
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2025-04-28 11:43:01 -0700

    added stratford

---
+++ wp-content/themes/stratford-wpcom/template-parts/footer/privacy-policy.php
@@ -0,0 +1,10 @@
+<?php declare( strict_types = 1 ); ?>
+<?php
+if ( function_exists( 'the_privacy_policy_link' ) ) {
+	?>
+	<div class="privacy-policy-container">
+	<?php the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' ); ?>
+	</div>
+	<?php
+}
+?>
