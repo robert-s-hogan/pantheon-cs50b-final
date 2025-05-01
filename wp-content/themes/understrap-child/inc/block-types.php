69939ad675e02ff9f820aaf5dfb1992f8bdec916
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2025-04-30 17:49:25 -0700

    debug block types

--- wp-content/themes/understrap-child/inc/block-types.php
+++ wp-content/themes/understrap-child/inc/block-types.php
@@ -1,17 +1,9 @@
 <?php
 // inc/block-types.php
-
 add_action( 'init', function() {
-  register_block_type( 'understrap-child/hero-section', [
-    'render_callback' => 'understrap_child_render_hero_section',
-    'attributes'      => [
-      'background' => [ 'type' => 'string' ],
-      'title'      => [ 'type' => 'string' ],
-      'buttonText' => [ 'type' => 'string' ],
-    ],
-    'category'        => 'hero',
-  ] );
-} );
+    register_block_type( __DIR__ . '/block-types/hero-section/block.json' );
+  } );
+  
 
 function understrap_child_render_hero_section( $attrs ) {
   return \UnderstrapChild\Atomic\render_template( 'organisms/hero', $attrs );
