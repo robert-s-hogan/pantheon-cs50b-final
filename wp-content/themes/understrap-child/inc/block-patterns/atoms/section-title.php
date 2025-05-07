85d0d788981c56b7e53c7501b67ba721af718265
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2025-05-06 19:13:18 -0700

    debug section title

--- wp-content/themes/understrap-child/inc/block-patterns/atoms/section-title.php
+++ wp-content/themes/understrap-child/inc/block-patterns/atoms/section-title.php
@@ -4,16 +4,16 @@
  */
 defined( 'ABSPATH' ) || exit;
 
-register_block_pattern(
-    'understrap-atom/section-title',
-    [
-        'title'      => __( 'Atom: Section Title', 'understrap-child' ),
-        'categories' => [ 'understrap-atom' ],
-        'content'    =>
-            '<!-- wp:heading {"level":2,"className":"section-title"} -->'
-          . '<h2 class="section-title">'
-          .   esc_html__( 'Section Title', 'understrap-child' )
-          . '</h2>'
-          . '<!-- /wp:heading -->',
-    ]
-);
+add_action( 'init', function() {
+    register_block_pattern(
+        'understrap/atom-section-title',
+        [
+            'title'      => esc_html__( 'Atom: Section Title', 'understrap-child' ),
+            'categories' => [ 'understrap-atom' ],
+            'content'    => '
+<!-- wp:heading {"level":2,"className":"section-title"} -->
+<h2 class="section-title">Your Section Title</h2>
+<!-- /wp:heading -->'
+        ]
+    );
+} );
