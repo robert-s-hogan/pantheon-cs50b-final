b58551a0d682c0dac9f869f2b0e2deb41306cfd8
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2025-05-16 08:22:37 -0700

    removed php.die DEBUG #2

--- wp-content/themes/understrap-child/page.php
+++ wp-content/themes/understrap-child/page.php
@@ -1,3 +1,25 @@
+
+
 <?php
-// page.php debugging
-die('✅ page.php is being used!');
+/**
+ * Child Theme Page Template
+ * This will ensure every Page loads the header and footer.
+ */
+get_header();  
+
+// wrapper (match parent theme’s markup)
+?>
+<div class="wrapper" id="page-wrapper">
+  <div id="content" class="container">
+    <?php
+    // Standard Loop: pull in your content-page.php
+    while ( have_posts() ) :
+      the_post();
+      get_template_part( 'loop-templates/content', 'page' );
+    endwhile;
+    ?>
+  </div><!-- #content -->
+</div><!-- #page-wrapper -->
+
+<?php
+get_footer(); 
