41a56db6df65d6d66569458ef4c5c9644b8085f3
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2025-05-17 16:36:50 -0700

    updated bootstrap header #6

--- wp-content/themes/understrap-child/global-templates/navbar-collapse-bootstrap5.php
+++ wp-content/themes/understrap-child/global-templates/navbar-collapse-bootstrap5.php
@@ -13,20 +13,31 @@ $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <?php if ( has_nav_menu( 'primary' ) ) : ?>
-  <nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary">
-    <div class="<?php echo esc_attr( get_theme_mod('understrap_container_type') ); ?>">
-      <?php get_template_part('global-templates/navbar-branding'); ?>
-      <button class="navbar-toggler" …></button>
+<nav class="navbar navbar-expand-lg bg-primary">
+  <div class="container d-flex justify-content-between align-items-center">
+    <?php the_custom_logo(); ?>
+    <a class="navbar-brand text-white ms-2" href="<?php echo home_url(); ?>">
+      <?php bloginfo( 'name' ); ?>
+    </a>
+    <button class="navbar-toggler border-0" type="button"
+            data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false"
+            aria-label="<?php esc_attr_e('Toggle navigation','understrap-child'); ?>">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
       <?php
-        wp_nav_menu([
-          'theme_location'  => 'primary',
-          'container'       => false,
-          'menu_class'      => 'navbar-nav ms-auto',
-          'fallback_cb'     => false,
-          'depth'           => 2,
-          'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-        ]);
+        wp_nav_menu( [
+          'theme_location' => 'primary',
+          'menu_class'     => 'navbar-nav',
+          'container'      => false,
+          'depth'          => 2,
+          'link_before'    => '<span class="nav-link-text">',
+          'link_after'     => '</span>',
+        ] );
       ?>
     </div>
-  </nav>
+  </div>
+</nav>
+
 <?php endif; ?>
