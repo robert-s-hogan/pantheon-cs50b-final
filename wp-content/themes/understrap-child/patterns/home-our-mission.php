5a83b8d5032da6cf5f8252290ba72bdae5dce5a2
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2025-05-18 16:04:10 -0700

    clear

---
+++ wp-content/themes/understrap-child/patterns/home-our-mission.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Mission – Home
+ * Slug: whited/home-mission
+ * Categories: home-page
+ * Inserter: yes
+ */
+?>
+
+<!-- wp:group {"align":"full","className":"mission-section"} -->
+  <!-- wp:group {"className":"section-inner"} -->
+    <!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} -->
+    <p class="has-text-align-center"><strong>At Whited Elementary PTO, our mission is simple:</strong></p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:paragraph {"align":"center","className":"mission-inner"} -->
+    <p class="has-text-align-center mission-inner">We strive to enhance the educational experience of every student by fostering a supportive community of parents, teachers, and staff. Through volunteer efforts, fundraising events, and open communication, we work together to create a positive environment where every family can thrive.</p>
+    <!-- /wp:paragraph -->
+  <!-- /wp:group -->
+<!-- /wp:group -->
